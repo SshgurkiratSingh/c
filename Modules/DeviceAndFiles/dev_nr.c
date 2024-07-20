a31c55b939293b55b6be252b3c6bcccc4700cef3
Gurkirat Singh <63462904+SshgurkiratSingh@users.noreply.github.com>
2024-07-20 10:00:26 +0530

    Automated Push

--- Modules/DeviceAndFiles/dev_nr.c
+++ Modules/DeviceAndFiles/dev_nr.c
@@ -10,17 +10,53 @@ MODULE_VERSION("0.1");
 * @brief It is called, when device file is opened
 
 */
-static int driver_open(struct inode *device_file) {}
-static int __init hello_init(void)
+static int driver_open(struct inode *device_file, struct file *instance)
 {
-    printk(KERN_INFO "Hello World kernel module loaded\n");
+    printk(KERN_INFO "dev_nr -open was called\n");
+    return 0;
+}
+/*
+* @brief It is called, when device file is opened
+
+*/
+static int driver_close(struct inode *device_file, struct file *instance)
+{
+    printk(KERN_INFO "dev_nr -close was called\n");
     return 0;
 }
 
-static void __exit hello_exit(void)
+static struct file_operations fops = {
+    .owner = THIS_MODULE,
+    .open = driver_open,
+    .release = driver_close};
+#define MYMAJOR 90
+#define MYMINOR 0
+static int __init
+hello_init(void)
 {
-    printk(KERN_WARNING "Goodbye World kernel module unloaded\n");
-}
+    int ret;
+    printk(KERN_INFO "Hello World kernel module loaded\n");
+    // Register the device
+    ret = register_chrdev(MYMAJOR, "mydev_generic", &fops);
+    if (retval > 0)
+    {
+        printk(KERN_ALERT "Can't get major number,registered with major:%d and minor:%d\n", reval >> 20, retval && 0xfffff);
+    }
+    else if (ret < 0)
+    {
+        printk(KERN_WARNING "Failed to register device\n");
+        return ret;
+    }
+    else
+    {
+        printk(KERN_ALERT "Registered device with major number %d\n ", MYMAJOR);
+    }
 
-module_init(hello_init);
-module_exit(hello_exit);
+    static void __exit hello_exit(void)
+    {
+        unregister_chrdev(MYMAJOR, "mydev_generic");
+        printk(KERN_WARNING "Goodbye World kernel module unloaded\n");
+    }
+
+    module_init(hello_init);
+    module_exit(hello_exit);
